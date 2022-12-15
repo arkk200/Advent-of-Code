fdf9c092a13212a8d07dc4b6cbe85c8ef6271208
arkk200 <arkk20000@gmail.com>
2022-12-15 14:00:18 +0900

    ADD :: Advent of Code 7

--- 2022/f.py
+++ 2022/f.py
@@ -1,33 +1,54 @@
-def path(dir: str):
-    size: int = 0
-    command: int = input()
-    if command.startswith('$'):
-        command: list = command.split(' ')
-        if command[1] == "cd":
-            if command[2] == "..":
-                return path(command[2]) + size
-        elif command[1] == "ls":
-            list_: str = None
-            while not (list_ := input()).startswith('$'):
-                if not list_.startswith("dir"):
-                    size += int(list_.split(' ')[0])
-                
-a = input()
-path(a.split()[2])
+history = []
+size_history = []
+sizes = []
 
+total:int = 0
+disk_space:int = 70000000
+required_space:int = 30000000
 
 def path(dir: str):
-    size: int = 0
+    global total, size_history, sizes
+    
+    size = 0
+    history.append(dir)
+    size_history.append(0)
+    
     while True:
-        command: int = input()
+        print([f"{v}:{size_history[i]}" for i, v in enumerate(history)], end = " ")
+        command: str = input()
         if command.startswith('$'):
             command: list = command.split(' ')
+
             if command[1] == "cd":
                 if command[2] == "..":
+                    sizes.append(size_history.pop())
+                    history.pop()
+                    total += size if size <= 100000 else 0
                     return size
                 else:
-                    size += path(command[2])
-        elif command[0] != "dir":
-            size += int(command[0])
+                    path(command[2])
+
+        elif not command.startswith('dir'):
+            size = int(command.split(' ')[0])
+            size_history = [i + size for i in size_history]
+
+def binary_search(sizes: list, rs: int) -> int:
+    i = 0
+    j = sizes.__len__() - 1
+    m = (i + j) // 2
+    while not (sizes[m] < rs and rs < sizes[m+1]) and i < j:
+        if sizes[m] > rs:
+            j = m
+        else:
+            i = m + 1
+        m = (i + j) // 2
+    return m + 1
+
 a = input()
-path(a.split()[2])
+path(a.split(' ')[2])
+sizes.sort()
+required_space = sizes[-1] - (disk_space - required_space)
+
+
+m = binary_search(sizes, required_space)
+print(sizes[m])
