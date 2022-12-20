e34bd6772208a450deaf33ff7bd5f53f06e71aee
arkk200 <arkk20000@gmail.com>
2022-12-20 13:23:51 +0900

    ADD :: Advent of Code Day 9-2

--- 2022/h.py
+++ 2022/h.py
@@ -1,13 +1,12 @@
 from copy import deepcopy
+from math import copysign
 
 visited: list[list[bool]] = [[False for _ in range(1000)] for _ in range(1000)]
-visited[0][0] = True
+visited[5][11] = True
 
 cnt: int = 1
 
-H: list[int] = [0, 0]
-
-T: list[int] = [0, 0]
+roap: list[list[int]] = [[5, 11] for _ in range(10)]
 
 dir: dict[str, list[int]] = {
     'R': [0, 1],
@@ -16,25 +15,45 @@ dir: dict[str, list[int]] = {
     'D': [-1, 0]
 }
 
-def isAway(a, b) -> bool:
-    # print(f"a: {a}, b: {b}")
-    # print(f"abs(a-b[0]): {abs(a[0] - b[0])}")
-    # print(f"abs(a-b[1]): {abs(a[1] - b[1])}")
-    return abs(a[0] - b[0]) == 2 or abs(a[1] - b[1]) == 2
+def print_visited() -> None:
+    global visited
+    for i, v in reversed(list(enumerate(visited))):
+        print(''.join([('#' if u == True else '.') if [i, j] not in roap else str(roap.index([i, j])) if roap.index([i, j]) != 0 else 'H' for j, u in enumerate(v)]))
+
+
+def move_roap(head_prev, roap) -> bool:
+    global cnt, visited
+    sign = lambda x: int(copysign(1, x)) if x != 0 else 0
+    vector: list[int] = [head_prev[0] - roap[1][0], head_prev[1] - roap[1][1]]
+    # print(f"vector: {vector}")
+    # print(roap)
+    # print(f"prev: {prev}")
+    for i in range(9):
+        # if abs(roap[i][0] - roap[i+1][0]) != 0 and abs(roap[i][1] - roap[i+1][1]) != 0:
+        vector = deepcopy([sign(roap[i][0] - roap[i+1][0]), sign(roap[i][1] - roap[i+1][1])])
+        # print(f"test[{i}]: {roap[i][0] - roap[i+1][0]} {roap[i][1] - roap[i+1][1]}")
+            # print(f"p[1] - r[i][1]: {prev[0] - roap[i][0]}, p[1] - r[i][1]: {prev[1] - roap[i][1]}")
+        if abs(roap[i][0] - roap[i+1][0]) == 2 or abs(roap[i][1] - roap[i+1][1]) == 2:
+            # print(f"abs1: {abs(roap[i][0] - roap[i+1][0])}, abs2: {abs(roap[i][1] - roap[i+1][1])}")
+            roap[i+1][0] += vector[0]
+            roap[i+1][1] += vector[1]
+            # print(f"moved pos: {roap[i+1]} vec: {vector}")
+            if i + 1 == 9 and visited[roap[i+1][0]][roap[i+1][1]] == False:
+                visited[roap[i+1][0]][roap[i+1][1]] = True
+                cnt += 1
+                # print(roap[i+1][0], roap[i+1][1])
+    # print_visited()
+    # print(f"after: {roap}")
 
 def move(direction, length) -> None:
-    global H, T, dir, cnt
+    global roap, dir, cnt
     for _ in range(length):
-        prev: list[int] = deepcopy(H)
-        H[0] += dir[direction][0]
-        H[1] += dir[direction][1]
-        # print(f"H: {H}")
-        if isAway(H, T):
-            T = deepcopy(prev)
-            # print(f"T: {T}")
-            if visited[T[0]][T[1]] == False: cnt += 1
-            visited[T[0]][T[1]] = True
-        # print("--------------------------------")
+        head_prev: list[int] = deepcopy(roap[0])
+        roap[0][0] += dir[direction][0]
+        roap[0][1] += dir[direction][1]
+        move_roap(head_prev, roap)
+        # for i, v in enumerate(roap):
+        # print("-------------------")
 
 while True:
     cmd: str = input()
